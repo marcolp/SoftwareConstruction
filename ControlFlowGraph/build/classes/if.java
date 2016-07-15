1917923d71fe715b3ffe75e0da870c400c8921a8
Marco Lopez <marcolp93@gmail.com>
2016-07-15 06:49:12 -0600

    Test file

---
+++ ControlFlowGraph/build/classes/if.java
@@ -0,0 +1,14 @@
+public class Test{
+	public static void main(String []args){
+		int x = 0;
+		if(true){
+			k = 1;
+		}
+		System.out.println(k);
+
+		for(int i = 10; i > 0; i--){
+			k++;
+			i+=0;
+		}
+	}
+}
